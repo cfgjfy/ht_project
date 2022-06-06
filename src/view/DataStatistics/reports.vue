fd09359a5f707c3eeba48ca51934e94ee36c9a56
cfgjfy <3250757557@qq.com>
2022-06-06 13:37:16 +0800

    update files

--- src/view/DataStatistics/reports.vue
+++ src/view/DataStatistics/reports.vue
@@ -1,13 +1,89 @@
+<!--
+ * @Author: cfgjfy
+ * @Date: 2022-05-30 20:05:53
+ * @LastEditors: cfgjfy
+ * @LastEditTime: 2022-06-06 13:20:43
+-->
 <template>
-    <div>DataReport</div>
+  <div id="main" style="width: 100%;height:100%;"></div>
 </template>
 
 <script>
+import * as echarts from 'echarts'
 export default {
-    name: "dataReport"
+  name: "dataReport",
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      var myChart = echarts.init(document.getElementById('main'));
+      // 绘制图表
+      myChart.setOption({
+        backgroundColor: '#2c343c',
+        title: {
+          text: 'Customized Pie',
+          left: 'center',
+          top: 20,
+          textStyle: {
+            color: '#ccc'
+          }
+        },
+        tooltip: {
+          trigger: 'item'
+        },
+        visualMap: {
+          show: false,
+          min: 80,
+          max: 600,
+          inRange: {
+            colorLightness: [0, 1]
+          }
+        },
+        series: [
+          {
+            name: 'Access From',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '50%'],
+            data: [
+              { value: 335, name: 'Direct' },
+              { value: 310, name: 'Email' },
+              { value: 274, name: 'Union Ads' },
+              { value: 235, name: 'Video Ads' },
+              { value: 400, name: 'Search Engine' }
+            ].sort(function (a, b) {
+              return a.value - b.value;
+            }),
+            roseType: 'radius',
+            label: {
+              color: 'rgba(255, 255, 255, 0.3)'
+            },
+            labelLine: {
+              lineStyle: {
+                color: 'rgba(255, 255, 255, 0.3)'
+              },
+              smooth: 0.2,
+              length: 10,
+              length2: 20
+            },
+            itemStyle: {
+              color: '#c23531',
+              shadowBlur: 200,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            },
+            animationType: 'scale',
+            animationEasing: 'elasticOut',
+            animationDelay: function (idx) {
+              return Math.random() * 200;
+            }
+          }
+        ]
+      });
+    }
+  }
 }
 </script>
 
 <style lang='less' scoped>
-
 </style>
